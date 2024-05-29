5385a4218221b4717fcfd287402d2ca74e3554c2
Chris Pinnock <chris_pinnock@mac.com>
2024-05-29 12:32:47 +0100

    ajinkya

--- index.ts
+++ index.ts
@@ -192,6 +192,39 @@ new TezosFaucet(
 
 // End of Paris Exploratory networks
 
+// Paris2net reboot test network
+/*const paris2net_chain = new TezosChain(
+  {
+    category: protocolCategory,
+    humanName: "Paris2net",
+    description: "Test Chain for Paris protocol",
+    activationBucket: activationBucket,
+    helmValuesFile: "networks/paris2net/values.yaml",
+    bakingPrivateKey: private_teztnets_baking_key,
+    bootstrapPeers: ["parisnet.tzinit.net"],
+    rpcUrls: [],
+    indexers: [],
+    chartRepoVersion: "7.1.2",
+    networkStakes: true,
+  },
+  provider
+)
+new TezosFaucet(
+  paris2net_chain.name,
+  {
+    namespace: paris2net_chain.namespace,
+    humanName: "Paris2net",
+    helmValuesFile: "networks/paris2net/faucet_values.yaml",
+    faucetPrivateKey: faucetPrivateKey,
+    faucetRecaptchaSiteKey: faucetRecaptchaSiteKey,
+    faucetRecaptchaSecretKey: faucetRecaptchaSecretKey,
+    chartRepoVersion: "7.1.2",
+  },
+  provider
+)
+*/
+
+
 // Parisnet test network
 const parisnet_chain = new TezosChain(
   {
