63c3d6d4c98fa5214633cb195cafa0947b1f582f
Chris Pinnock <chris_pinnock@mac.com>
2024-06-25 16:56:46 +0100

    make purposes clearer

--- teztnets_xyz_page/release.py
+++ teztnets_xyz_page/release.py
@@ -21,10 +21,10 @@ for network_name in networks:
 # group by category for human rendering
 # Order manually. Start with long-running.
 category_desc = {
-    "Long-running Teztnets": "If you are not sure, pick this one.",
-    "Protocol Teztnets": "Testnets deployed specifically to test new Tezos protocol proposals.",
+    "Long-running Teztnets": "These networks are long-running test networks that are similar to mainnet. They run the same protocol as mainnet but switch over to the next protocol earlier than mainnet for testing. If you are developing applications, you probably want to develop on these networks."
+    "Protocol Teztnets": "These test networks are deployed specifically to test individual Tezos protocols. There is usually one for the currently deployed protocol and one for the next protocol. If you are developing your application for the next protocol, you will want to use that test network."
 #    "Feature Teztnets": "Testnets deployed specifically to test specific Tezos features.",
-    "Periodic Teztnets": "Testnets that restart regularly and track the development of the master branch of [Octez repo](https://gitlab.com/tezos/tezos/).\n \n☠️ You probably don't want this unless you are a core protocol developer.",
+    "Periodic Teztnets": "These test networks restart regularly and track the development of the master branch of [Octez repo](https://gitlab.com/tezos/tezos/).\n \n☠️ You probably don't want to use these unless you are a core protocol developer.",
 }
 
 nested_teztnets = {
