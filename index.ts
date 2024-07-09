2ca2289e2165731af139dff7f71851624bd2a09a
Chris Pinnock <chris_pinnock@mac.com>
2024-07-09 20:19:50 +0100

    fix

--- index.ts
+++ index.ts
@@ -89,7 +89,7 @@ const weeklynet_chain = new TezosChain(
     category: periodicCategory,
     humanName: "Weeklynet",
     description:
-      "A testnet that restarts every Wednesday launched from tezos/tezos master branch. It runs Oxford for 4 cycles then upgrades to proto Alpha.",
+      "A testnet that restarts every Wednesday launched from tezos/tezos master branch. It runs ParisC for 4 cycles then upgrades to proto Alpha.",
     schedule: "0 0 * * WED",
     activationBucket: activationBucket,
     bootstrapContracts: [
