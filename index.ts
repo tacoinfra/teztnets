bfd2a36860f47f9d0132ce37d474a4f4b276e381
Chris Pinnock <chris_pinnock@mac.com>
2024-07-23 18:02:30 +0100

    put back for now

--- index.ts
+++ index.ts
@@ -351,8 +351,8 @@ const ghostnetNetwork = {
 }
 
 export const networks = {
-//  ...getNetworks([weeklynet_chain, pariscnet_chain, betanet1_chain]),
-  ...getNetworks([pariscnet_chain, betanet1_chain]),
+  ...getNetworks([weeklynet_chain, pariscnet_chain, betanet1_chain]),
+//  ...getNetworks([pariscnet_chain, betanet1_chain]),
 //  ...getNetworks([pariscnet_chain]),
   ...{ ghostnet: ghostnetNetwork },
 }
@@ -419,8 +419,8 @@ const mainnetMetadata = {
 }
 
 export const teztnets = {
-//  ...getTeztnets([weeklynet_chain, pariscnet_chain, betanet1_chain]),
-  ...getTeztnets([pariscnet_chain, betanet1_chain]),
+  ...getTeztnets([weeklynet_chain, pariscnet_chain, betanet1_chain]),
+//  ...getTeztnets([pariscnet_chain, betanet1_chain]),
   ...{ ghostnet: ghostnetTeztnet, mainnet: mainnetMetadata },
 }
 
