d6d470900721501c11829ca7052bc35f608273be
Chris Pinnock <chris_pinnock@mac.com>
2024-11-12 11:08:35 +0000

    pull out

--- index.ts
+++ index.ts
@@ -187,7 +187,7 @@ new TezosFaucet(
 
 // Quebecnet First Blood Part III
 //
-const quebecnet_chain = new TezosChain(
+/*const quebecnet_chain = new TezosChain(
   {
     category: protocolCategory,
     humanName: "Quebecnet",
@@ -215,7 +215,7 @@ new TezosFaucet(
   },
   provider
 )
-
+*/
 
 // Dal-test
 const dqnet_chain = new TezosChain(
@@ -366,7 +366,7 @@ const ghostnetNetwork = {
 }
 
 export const networks = {
-  ...getNetworks([dqnet_chain, quebecnet_chain, weeklynet_chain, pariscnet_chain]),
+  ...getNetworks([dqnet_chain, weeklynet_chain, pariscnet_chain]),
   ...{ ghostnet: ghostnetNetwork },
 }
 
@@ -430,7 +430,7 @@ const mainnetMetadata = {
 }
 
 export const teztnets = {
-  ...getTeztnets([dqnet_chain, quebecnet_chain, weeklynet_chain, pariscnet_chain]),
+  ...getTeztnets([dqnet_chain, weeklynet_chain, pariscnet_chain]),
   ...{ ghostnet: ghostnetTeztnet, mainnet: mainnetMetadata },
 }
 
