0074e17a92749288cebb3c0d8fefb13f024c66ff
Chris Pinnock <chris_pinnock@mac.com>
2025-01-06 20:22:14 +0000

    update roller

--- index.ts
+++ index.ts
@@ -120,7 +120,7 @@ new TezosFaucet(
 // * heavy usage on the RPC endpoint requires a more elaborate setup
 //   with archive/rolling nodes, NGINX path filtering and rate limiting.
 // Consequently, we made a special class "TezosNodes" for the purpose.
-const ghostnetRollingVersion = "octez-v20.3";
+const ghostnetRollingVersion = "octez-v21.1";
 const ghostnetArchiveVersion = "octez-v20.3";
 const ghostnet_chain = new TezosNodes(
   "ghostnet-nodes",
