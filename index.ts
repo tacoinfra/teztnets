7a1c087c8e23f43c0a1370f61d1f95c0090f7dba
Chris Pinnock <chris_pinnock@mac.com>
2025-10-22 16:08:40 +0100

    fix weeklynet

--- index.ts
+++ index.ts
@@ -81,6 +81,7 @@ new gcp.dns.RecordSet("teztnetsComSiteRecord", {
 });
 
 // Weeklynet - restarts Wednesdays
+
 const weeklynet_chain = new TezosChain(
   {
     category: periodicCategory,
@@ -114,6 +115,7 @@ new TezosFaucet(
   },
   provider
 )
+
 
 // Ghostnet is different from the other testnets:
 // * launched long time ago, launch code is not in the active code path
