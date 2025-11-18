c413391c72c54cd917d0e5670d17d53fb0e8a5c1
Chris Pinnock <chris_pinnock@mac.com>
2025-11-18 21:45:47 +0000

    Tallin

--- scripts/tallinn_switch.sh
+++ scripts/tallinn_switch.sh
@@ -1,11 +1,12 @@
 
 
-PROTO=PtSeouLouXkxhg39oWzjxDWaCydNfR3RxCUrNe4Q9Ro8BTehcbh
+NETWORK=tallinnnet
+PROTO=PtTALLiNtPec7mE7yY4m3k26J8Qukef3E3ehzhfXgFZKGtDdAXu
 KEY=qdictator
 FAUCET=farrah
-ENDPOINT="https://rpc.seoulnet.teztnets.com"
+ENDPOINT="https://rpc.${NETWORK}.teztnets.com"
 
-echo "Switching to $PROTO"
+echo "Switching ${NETWORK} to $PROTO"
 echo "CTRL+C now if wrong"
 sleep 5
 
@@ -13,7 +14,6 @@ sleep 5
 sleep 5
 ~/tezos/tezos/octez-client -E ${ENDPOINT} transfer 1 from ${KEY} to $FAUCET --burn-cap 0.06425
 sleep 5
-
 
 echo "octez-client -E ${ENDPOINT} submit proposals for $KEY $PROTO --force"
 #octez-client -E ${ENDPOINT} submit proposals for $KEY $PROTO --force
