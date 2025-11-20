caf7232ae78d8c49a210a4d0d12884c428c6db29
Chris Pinnock <chris_pinnock@mac.com>
2025-11-20 12:03:57 +0000

    move weeklynet to 8.0.3

--- index.ts
+++ index.ts
@@ -96,7 +96,7 @@ const weeklynet_chain = new TezosChain(
     helmValuesFile: "networks/weeklynet/values.yaml",
     bakingPrivateKey: private_teztnets_baking_key,
     // chartPath: "networks/weeklynet/tezos-k8s", // point to a submodule, to run unreleased tezos-k8s code
-    chartRepoVersion: "8.0.2", // point to a release of tezos-k8s. This should be the default state.
+    chartRepoVersion: "8.0.3", // point to a release of tezos-k8s. This should be the default state.
     bootstrapPeers: ["weeklynet.tzinit.org"],
     //    alias: "weeklynet",
   },
