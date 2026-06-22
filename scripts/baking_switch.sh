a96f2fd2e33dd5ce8662485b99f6932fed5646a8
Chris Pinnock <chris_pinnock@mac.com>
2026-06-22 14:30:31 +0100

    switcher

---
+++ scripts/baking_switch.sh
@@ -0,0 +1,18 @@
+
+PROTO=PsUshuai9QapM5TGj1JpuVGkdxz5GykdnEvS6Rh8SUVrARvZLCY
+KEY=qdictator
+FAUCET=farrah
+ENDPOINT="https://rpc.bakingnet.teztnets.com"
+
+echo "Switching to $PROTO"
+echo "CTRL+C now if wrong"
+sleep 5
+
+~/tezos/tezos/octez-client -E ${ENDPOINT} transfer 2 from ${FAUCET} to $KEY --burn-cap 0.06425
+sleep 5
+~/tezos/tezos/octez-client -E ${ENDPOINT} transfer 1 from ${KEY} to $FAUCET --burn-cap 0.06425
+sleep 5
+
+
+echo "octez-client -E ${ENDPOINT} submit proposals for $KEY $PROTO --force"
+#octez-client -E ${ENDPOINT} submit proposals for $KEY $PROTO --force
